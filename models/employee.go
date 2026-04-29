ed730f950afbc1e41a30c5caca169bc894813e0d
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-29 12:59:34 +0530

    Create employee.go

---
+++ models/employee.go
@@ -0,0 +1,9 @@
+package models
+
+type Employee struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Position string `json:"position"`
+	Salary   int    `json:"salary"`
+}
